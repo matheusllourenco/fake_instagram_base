9bb3c5f88a8857ce8b3a9178be951beb56b795b0
tsunodajapa <gabrieltsunoda@gmail.com>
2022-08-23 21:40:06 -0300

    feat: change placeholder

---
+++ js/exercico3.js
@@ -0,0 +1,5 @@
+// const inputName = document.querySelector('form.form-auth.card input[name="name"]');
+// const inputName = document.querySelector('input:nth-child(3)');
+const inputName = document.querySelector('input:nth-of-type(2)');
+
+inputName.placeholder = 'Digite seu primeiro nome';
